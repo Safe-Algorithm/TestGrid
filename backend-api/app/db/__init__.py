64b52a6298717ce4bc386a357d69b51d40048fea
smart-programmer <albrns1123488@outlook.com>
2023-12-25 20:02:29 +0300

    feat: initial setup of backend api with readme updates

---
+++ backend-api/app/db/__init__.py
@@ -0,0 +1,11 @@
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from beanie import init_beanie
+
+from app.core.config import settings
+
+async def init_db():
+    client: AsyncIOMotorClient = AsyncIOMotorClient(settings.MONGO_CONNECTION_URL.unicode_string())
+    db: AsyncIOMotorDatabase = client.get_database(settings.MONGO_DB_NAME)
+
+    await init_beanie(database=db, document_models=[])
+
