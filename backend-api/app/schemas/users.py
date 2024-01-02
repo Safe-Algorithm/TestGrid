f7f51a7f88f6430c6ed23e2d8b7bd446eab3d4eb
smart-programmer <albrns1123488@outlook.com>
2024-01-02 11:58:06 +0300

    feat: create login route

--- backend-api/app/schemas/users.py
+++ backend-api/app/schemas/users.py
@@ -1,10 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+from app.enums.users import UserTypesEnum
 
 
 class BaseUser(BaseModel):
+    email: EmailStr
+
+class UserOutDefault(BaseUser):
     username: str
-    email: str
 
-
-class UserInRegister(BaseUser):
+class UserInLogin(BaseUser):
     password: str
+
+class UserInRegister(UserOutDefault):
+    username: str
+    password: str
+
+class TokenUser(UserOutDefault):
+    created_at: datetime
+    updated_at: datetime
+    user_type: UserTypesEnum
+    active: bool
+    disabled: bool
+
+class AccessToken(BaseModel):
+    access_token: str
+ 
--- backend-api/app/utils/users.py
+++ backend-api/app/utils/users.py
@@ -1,15 +1,25 @@
+import jwt
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
 
+from app.core.config import settings
 from app.logs.log_conf import Logger
 
 logger = Logger(__name__)
 
 pwd_context = CryptContext(schemes=["bcrypt"], default="bcrypt", deprecated="auto")
 
-def verify_password(plain_password, hashed_password) -> bool:
+async def verify_password(plain_password, hashed_password) -> bool:
     logger.info(f"verifying password hash of password: {plain_password} with hash: {hashed_password}")
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password) -> str:
+async def get_password_hash(password) -> str:
     logger.info(f"hashing password {password}")
     return pwd_context.hash(password)
+
+async def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire_date = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire_date})
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET, algorithm="HS256")
+    return encoded_jwt
