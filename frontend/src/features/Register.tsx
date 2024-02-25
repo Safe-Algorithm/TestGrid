9e17b0f5ebfbebd1d3fc85ba3b95c4b5f85c4df0
Enma <hm.morashid@gmail.com>
2024-02-25 10:07:11 +0300

    feat: setup register functionality

--- frontend/src/features/Register.tsx
+++ frontend/src/features/Register.tsx
@@ -1,8 +1,12 @@
-const host = (String = import.meta.env.VITE_SERVER_HOST);
-const port = (String = import.meta.env.VITE_SERVER_PORT);
-const API_KEY = (String = import.meta.env.VITE_API_KEY);
+import { useState } from "react";
+
+const host: string = import.meta.env.VITE_SERVER_HOST;
+const port: string = import.meta.env.VITE_SERVER_PORT;
+const API_KEY: string = import.meta.env.VITE_API_KEY;
 
 const Register = () => {
+  const [error, setError] = useState("");
+  const [isError, setIsError] = useState(false);
   const user = {
     email: String,
     username: String,
@@ -20,28 +24,62 @@ const Register = () => {
         body: JSON.stringify(user),
       });
 
-      console.log(res.status);
+      /*
+       returns 409 in the following error scenarios:
+       - username is exist
+       - email is exist
+       - username is missing
+      */
+      if (res.status === 409) {
+        setError("Username or Email already exist");
+        setIsError(true);
+      } else if (res.status === 422) {
+        // if email is missing
+        setError("Invalid email!");
+        setIsError(true);
+      } else {
+        setError("");
+        setIsError(true);
+      }
     } catch (error) {
-      throw new Error(`${error}`);
-    } finally {
-      console.log(`this is finally`);
+      setError("Something went wrong!");
+      throw new Error("Something went wrong!");
     }
   };
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     user.email = e.target.email.value;
     user.username = e.target.username.value;
     user.password = e.target.password.value;
 
-    fetchAPI();
+    await fetchAPI();
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" name="email" placeholder="user@example.com" />
-      <input type="username" name="username" placeholder="@username" />
-      <input type="password" name="password" placeholder="password" />
-      <button type="submit">register</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          required
+          placeholder="user@example.com"
+        />
+        <input
+          type="username"
+          name="username"
+          required
+          placeholder="@username"
+        />
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="password"
+        />
+        <button type="submit">register</button>
+      </form>
+      {isError && <p>{error}</p>}
+    </>
   );
 };
 
