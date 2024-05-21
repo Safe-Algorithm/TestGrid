3126136f9791c7859490eb78f1df01be1764e5e9
Omir <omirabbasnw@gmail.com>
2024-05-21 17:02:38 +0300

    refactor: fix bugs on the website

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export default function Header() {
   const navigate = useNavigate();
   function handleOnClick() {
-    navigate("/test");
+    navigate("/test/penetration");
   }
   return (
     <Container>
