fbc6d47372a7dc4821a8d505bb56d4739eb8bc8a
Omir <omirabbasnw@gmail.com>
2024-05-23 05:06:46 +0300

    feat: add user authentication

--- frontend/src/features/PenOptions.tsx
+++ frontend/src/features/PenOptions.tsx
@@ -1,8 +1,34 @@
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+
 import Container from "../components/Container";
 import Heading from "../components/Heading";
 import PenOption from "../components/PenOption";
+import { useNavigate } from "react-router-dom";
 
 export default function PenOptions() {
+  const host = import.meta.env.VITE_SERVER_HOST;
+  const port = import.meta.env.VITE_SERVER_PORT;
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    async function authenticateUser() {
+      const accessToken = Cookies.get("accessToken");
+      const response = await fetch(
+        `http://${host}:${port}/api/v1/auth/is-authenticated`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      if (response.status != 200) {
+        navigate("/login");
+      }
+    }
+    authenticateUser();
+  });
   return (
     <div className="bg-neutral-100 w-full h-full md:h-screen">
       <Container>
